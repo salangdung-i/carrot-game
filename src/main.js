9d885e92b105a9c26a401ae90b1914248acaebfb
salangdung-i <dladmswl92@naver.com>
2021-11-24 22:14:30 +0900

    builder patten apply

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 'use strict';
 import PopUp from './popup.js';
-import Game from './game.js';
+import GameBuilder from './game.js';
 
 const gameFinishBanner = new PopUp();
-const game = new Game(20, 20, 20);
+const game = new GameBuilder()
+  .withGameDuration(5)
+  .withCarrotCount(3)
+  .withBugCount(3)
+  .build();
+
 game.setGameStopListener(reason => {
   let message;
 
